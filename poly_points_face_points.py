ad343f5f76949f25176e569493470ef264f73b09
akashkmr27089 <akashkmr27089@gmail.com>
2020-07-06 13:21:48 +0530

    Initial

---
+++ poly_points_face_points.py
@@ -0,0 +1,103 @@
+import cv2
+import numpy as np
+import dlib
+
+detector = dlib.get_frontal_face_detector()
+
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+img = cv2.imread("face.png")
+
+gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+faces = detector(gray)
+
+for face in faces:
+	x1 = face.left()
+	y1 = face.top()
+	x2 = face.right()
+	y2 = face.bottom()
+	
+	landmarks = predictor(image = gray, box=face)
+	
+	# Jaw Points 0-16
+	doc = []
+	for n in range(0,17):
+		x = landmarks.part(n).x
+		y = landmarks.part(n).y
+		doc.append([x,y])
+	pts = np.array(doc).astype(np.int32)
+	pts = pts.reshape((-1,1,2))
+	cv2.polylines(img, [pts], False, (0,255,255))
+
+	# Right Brow Points 17-21	
+	doc = []	
+	for n in range(17,22):
+		x = landmarks.part(n).x
+		y = landmarks.part(n).y
+		doc.append([x,y])
+	pts = np.array(doc).astype(np.int32)
+	pts = pts.reshape((-1,1,2))
+	cv2.polylines(img, [pts], False, (0,255,255))
+
+	#Left Brow Points 22-26	
+	doc = []	
+	for n in range(22,27):
+		x = landmarks.part(n).x
+		y = landmarks.part(n).y
+		doc.append([x,y])
+	pts = np.array(doc).astype(np.int32)
+	pts = pts.reshape((-1,1,2))
+	cv2.polylines(img, [pts], False, (0,255,255))
+
+	#Nose Points 27-35
+	doc = []	
+	for n in range(27,36):
+		x = landmarks.part(n).x
+		y = landmarks.part(n).y
+		doc.append([x,y])
+	pts = np.array(doc).astype(np.int32)
+	pts = pts.reshape((-1,1,2))
+	cv2.polylines(img, [pts], False, (0,255,255))
+
+	#Right Eye 36-41
+	doc = []	
+	for n in range(36,42):
+		x = landmarks.part(n).x
+		y = landmarks.part(n).y
+		doc.append([x,y])
+	pts = np.array(doc).astype(np.int32)
+	pts = pts.reshape((-1,1,2))
+	cv2.polylines(img, [pts], False, (0,255,255))
+
+	#Left Eye 42-47
+	doc = []	
+	for n in range(42,48):
+		x = landmarks.part(n).x
+		y = landmarks.part(n).y
+		doc.append([x,y])
+	pts = np.array(doc).astype(np.int32)
+	pts = pts.reshape((-1,1,2))
+	cv2.polylines(img, [pts], False, (0,255,255))
+	
+	#Mouth Points 48-60
+	doc = []	
+	for n in range(48,61):
+		x = landmarks.part(n).x
+		y = landmarks.part(n).y
+		doc.append([x,y])
+	pts = np.array(doc).astype(np.int32)
+	pts = pts.reshape((-1,1,2))
+	cv2.polylines(img, [pts], False, (0,255,255))
+
+	#Lips Points 61-67
+	doc = []	
+	for n in range(61,68):
+		x = landmarks.part(n).x
+		y = landmarks.part(n).y
+		doc.append([x,y])
+	pts = np.array(doc).astype(np.int32)
+	pts = pts.reshape((-1,1,2))
+	cv2.polylines(img, [pts], False, (0,255,255))
+
+cv2.imshow(winname="Face", mat=img)
+cv2.waitKey(delay=0)
+cv2.destroyAllWindows()
---
+++ test.py
@@ -0,0 +1,30 @@
+import cv2
+import dlib
+# Load the detector
+detector = dlib.get_frontal_face_detector()
+# read the image
+img = cv2.imread("./face.png")
+# Convert image into grayscale
+
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
+# Use detector to find landmarks
+faces = detector(gray)
+for face in faces:
+    x1 = face.left() # left point
+    y1 = face.top() # top point
+    x2 = face.right() # right point
+    y2 = face.bottom() # bottom point
+    # Draw a rectangle
+    #cv2.rectangle(img=img, pt1=(x1, y1), pt2=(x2, y2), color=(0, 255, 0), thickness=4)
+    landmarks = predictor(image=gray, box=face)
+    for n in range(0,68):
+	x = landmarks.part(n).x
+	y = landmarks.part(n).y
+# show the image
+cv2.imshow(winname="Face", mat=img)
+# Wait for a key press to exit
+cv2.waitKey(delay=0)
+# Close all windows
+cv2.destroyAllWindows()	
